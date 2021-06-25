01e14a3369bd0e552a4f41f1214d628b864e7ee3
dinel <solahuddin.hafid.tomaballo@gmail.com>
2021-06-25 19:29:42 +0800

    add ui slice

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
-
+import { Route, Switch } from "react-router-dom";
+import { useSelector } from "react-redux";
 import Fulnav from "./components/nav/Fulnav";
 
 export default function App() {
+  // const token = useSelector((state) => state.auth.token);
   return (
     <>
       <Fulnav />
-      <div className="xl:mt-0 mt-20">
-        <h1>header</h1>
-      </div>
+      <Switch>
+        <Route path="/" exact>
+          gdfgfdg
+        </Route>
+        <Route path="/perjalanan" exact>
+          <div className="xl:mt-0 mt-20">
+            <h1>header</h1>
+          </div>
+        </Route>
+      </Switch>
     </>
   );
 }

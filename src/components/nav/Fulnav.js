aceb75004a7a9a3effd13bbd29b21cd870d6db43
dinel <solahuddin.hafid.tomaballo@gmail.com>
2021-06-25 18:52:41 +0800

    fix teh sidenav

--- src/components/nav/Fulnav.js
+++ src/components/nav/Fulnav.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 import logo from "../../assets/J.png";
 export default function Fulnav() {
@@ -13,15 +14,15 @@ export default function Fulnav() {
             <div className="flex items-center justify-between">
               {/* for branc nav*/}
               <div className="flex w-full sm:w-auto items-center sm:items-stretch justify-end sm:justify-start">
-                <div className="flex items-center w-24">
+                <Link to="/" className="flex items-center w-24">
                   <img src={logo} alt="logo" className="h-8 mr-1" />
                   <p className="text-lg tracking-wide font-medium">Jokkaki</p>
-                </div>
+                </Link>
               </div>
               <div className="flex">
                 <div className="hidden xl:flex md:mr-6 xl:mr-16">
-                  <a
-                    href="javascript: void(0)"
+                  <Link
+                    to="/perjalanan"
                     className="flex px-5 items-center py-6 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none transition duration-150 ease-in-out"
                   >
                     <span className="mr-2">
@@ -45,9 +46,9 @@ export default function Fulnav() {
                       </svg>
                     </span>
                     Perjalanan
-                  </a>
-                  <a
-                    href="javascript: void(0)"
+                  </Link>
+                  <Link
+                    to="/penunjuk"
                     className="flex px-5 items-center py-6 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none transition duration-150 ease-in-out"
                   >
                     <span className="mr-2">
@@ -68,7 +69,7 @@ export default function Fulnav() {
                       </svg>
                     </span>
                     Guided
-                  </a>
+                  </Link>
                 </div>
                 <div className="hidden xl:flex items-center">
                   <div className="relative md:mr-6 my-2">
@@ -108,7 +109,7 @@ export default function Fulnav() {
                       {profile && (
                         <ul className="p-2 w-40 border-r bg-gray-50 absolute rounded right-0 shadow-lg top-0 mt-16 ">
                           <li className="cursor-pointer text-gray-600 text-sm leading-3 tracking-normal py-2 hover:text-indigo-700 focus:text-indigo-700 focus:outline-none">
-                            <div className="flex items-center">
+                            <Link to="/akunku" className="flex items-center">
                               <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 className="icon icon-tabler icon-tabler-user"
@@ -125,28 +126,30 @@ export default function Fulnav() {
                                 <circle cx={12} cy={7} r={4} />
                                 <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
                               </svg>
-                              <span className="ml-2">My Profile</span>
-                            </div>
+                              <span className="ml-2">Akun ku</span>
+                            </Link>
                           </li>
                           <li className="cursor-pointer text-gray-600 text-sm leading-3 tracking-normal mt-2 py-2 hover:text-indigo-700 focus:text-indigo-700 focus:outline-none flex items-center">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              className="icon icon-tabler icon-tabler-help"
-                              width={20}
-                              height={20}
-                              viewBox="0 0 24 24"
-                              strokeWidth="1.5"
-                              stroke="currentColor"
-                              fill="none"
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                            >
-                              <path stroke="none" d="M0 0h24v24H0z" />
-                              <circle cx={12} cy={12} r={9} />
-                              <line x1={12} y1={17} x2={12} y2="17.01" />
-                              <path d="M12 13.5a1.5 1.5 0 0 1 1 -1.5a2.6 2.6 0 1 0 -3 -4" />
-                            </svg>
-                            <span className="ml-2">Help Center</span>
+                            <Link to="/bantuan" className="flex items-center">
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="icon icon-tabler icon-tabler-help"
+                                width={20}
+                                height={20}
+                                viewBox="0 0 24 24"
+                                strokeWidth="1.5"
+                                stroke="currentColor"
+                                fill="none"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                              >
+                                <path stroke="none" d="M0 0h24v24H0z" />
+                                <circle cx={12} cy={12} r={9} />
+                                <line x1={12} y1={17} x2={12} y2="17.01" />
+                                <path d="M12 13.5a1.5 1.5 0 0 1 1 -1.5a2.6 2.6 0 1 0 -3 -4" />
+                              </svg>
+                              <span className="ml-2">Pusat Bantuan</span>
+                            </Link>
                           </li>
                         </ul>
                       )}
@@ -212,10 +215,10 @@ export default function Fulnav() {
                   </svg>
                 )}
               </div>
-              <div className="flex items-center w-24">
+              <Link to="/" className="flex items-center w-24">
                 <img src={logo} alt="logo" className="h-6 mr-1" />
                 <p className="text-lg tracking-wide font-medium">Jokkaki</p>
-              </div>
+              </Link>
             </div>
 
             <div className="flex items-center">
@@ -266,12 +269,16 @@ export default function Fulnav() {
                   <div>
                     <div className="mt-6 flex w-full items-center justify-between">
                       <div className="flex items-center justify-between w-full">
-                        <div className="flex items-center w-24">
+                        <Link
+                          to="/"
+                          onClick={() => setShow(!show)}
+                          className="flex items-center w-24"
+                        >
                           <img src={logo} alt="logo" className="h-6 mr-1" />
                           <p className="text-lg tracking-wide font-medium">
                             Jokkaki
                           </p>
-                        </div>
+                        </Link>
                         <div
                           id="cross"
                           className="text-gray-800"
@@ -298,8 +305,12 @@ export default function Fulnav() {
                     </div>
 
                     <ul className="f-m-m">
-                      <a className="cursor-pointer">
-                        <li className="text-gray-800 pt-10 hover:text-indigo-700">
+                      <li className="text-gray-800 pt-10 hover:text-indigo-700">
+                        <Link
+                          to="/perjalana"
+                          onClick={() => setShow(!show)}
+                          className="cursor-pointer"
+                        >
                           <div className="flex items-center">
                             <div className="w-6 h-6 md:w-8 md:h-8 ">
                               <svg
@@ -341,10 +352,15 @@ export default function Fulnav() {
                               Perjalanan
                             </p>
                           </div>
-                        </li>
-                      </a>
-                      <a className="cursor-pointer">
-                        <li className="text-gray-800 pt-8 hover:text-indigo-700">
+                        </Link>
+                      </li>
+
+                      <li className="text-gray-800 pt-8 hover:text-indigo-700">
+                        <Link
+                          to="/penunjuk"
+                          onClick={() => setShow(!show)}
+                          className="cursor-pointer"
+                        >
                           <div className="flex items-center justify-between">
                             <div className="flex items-center">
                               <div className="w-6 h-6 md:w-8 md:h-8 ">
@@ -367,10 +383,14 @@ export default function Fulnav() {
                               </p>
                             </div>
                           </div>
-                        </li>
-                      </a>
-                      <a className="cursor-pointer">
-                        <li className="text-gray-800 pt-8 hover:text-indigo-700">
+                        </Link>
+                      </li>
+                      <li className="text-gray-800 pt-8 hover:text-indigo-700">
+                        <Link
+                          to="/akunku"
+                          onClick={() => setShow(!show)}
+                          className="cursor-pointer"
+                        >
                           <div className="flex items-center justify-between">
                             <div className="flex items-center">
                               <div className="w-6 h-6 md:w-8 md:h-8 ">
@@ -394,10 +414,14 @@ export default function Fulnav() {
                               </p>
                             </div>
                           </div>
-                        </li>
-                      </a>
-                      <a className="cursor-pointer">
-                        <li className="text-gray-800 hover:text-indigo-700 pt-8">
+                        </Link>
+                      </li>
+                      <li className="text-gray-800 hover:text-indigo-700 pt-8">
+                        <Link
+                          to="/bantuan"
+                          onClick={() => setShow(!show)}
+                          className="cursor-pointer"
+                        >
                           <div className="flex items-center justify-between">
                             <div className="flex items-center">
                               <div className="w-6 h-6 md:w-8 md:h-8 ">
@@ -422,8 +446,8 @@ export default function Fulnav() {
                               </p>
                             </div>
                           </div>
-                        </li>
-                      </a>
+                        </Link>
+                      </li>
                     </ul>
                   </div>
                   <div className="w-full pt-4">
